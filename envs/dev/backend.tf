043531f5041d068262e555cf16632ec218351dce
“lizandambs” <lizndambs@gmail.com>
2026-06-18 18:42:54 -0400

    feat(terraform): add terraform module

---
+++ envs/dev/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "bucket-name"
+    key = "envs/dev/terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
---
+++ modules/vpc/main.tf
@@ -0,0 +1,9 @@
+resource "aws_vpc" "v1" {
+  cidr_block = var.vpc_cidr
+  instance_tenancy = var.instance_tenancy
+
+
+  tags = {
+    Name = var.vpc_name
+  }
+}
